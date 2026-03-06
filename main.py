df62765b178a3581ac0c1a6f9c8d2cae431f3088
Krishna Pahune <krishnapahune4@gmail.com>
2026-03-06 18:50:44 +0530

    post method

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello Dear"}
+
+@app.get("/about")
+def root():
+    return {"Hi there"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"name: {payload['name']} roll: {payload['roll']}"}
