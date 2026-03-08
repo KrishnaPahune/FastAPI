21281dc1e018d890c8f3d6657e439b58c36ca237
Krishna Pahune <krishnapahune4@gmail.com>
2026-03-08 17:44:02 +0530

    learned data validation using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    name: str
+    roll: str
+    published: bool = True
+    rating: Optional[int] = None
+    
 
 @app.get("/")
 def root():
@@ -12,6 +21,8 @@ def root():
     return {"Hi there"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"name: {payload['name']} roll: {payload['roll']}"}
+def create_posts(post: Post):
+    name = post.name
+    roll = post.roll
+    print(post)
+    return {"data": post}
