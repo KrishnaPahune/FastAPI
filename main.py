6e28d4766b3969b1336dc0cbe4bb43e46ea93f39
Krishna Pahune <krishnapahune4@gmail.com>
2026-03-10 21:58:47 +0530

    adding and fetching new post

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+import random
 
 app = FastAPI()
 
 class Post(BaseModel):
-    name: str
-    roll: str
+    title: str
+    content: str
     published: bool = True
     rating: Optional[int] = None
+    id: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
     
 
 @app.get("/")
 def root():
     return {"message": "Hello Dear"}
 
-@app.get("/about")
+@app.get("/posts")
 def root():
-    return {"Hi there"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    name = post.name
-    roll = post.roll
-    print(post)
+    post.id= random.randint(0, 100000000)
+    my_posts.append(post)
     return {"data": post}
