eeedadbd06d63779b0e5209a41c44c9af61cae98
Krishna Pahune <krishnapahune4@gmail.com>
2026-03-11 19:28:02 +0530

    fetching exact post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 import random
@@ -13,19 +12,38 @@ class Post(BaseModel):
     rating: Optional[int] = None
     id: Optional[int] = None
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
-    
+
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "favorite foods", "content": "I like pizza", "id": 2}
+]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 @app.get("/")
-def root():
+def home():
     return {"message": "Hello Dear"}
 
+
 @app.get("/posts")
-def root():
+def get_posts():
     return {"data": my_posts}
+
 
 @app.post("/posts")
 def create_posts(post: Post):
-    post.id= random.randint(0, 100000000)
-    my_posts.append(post)
+    post.id = random.randint(0, 100000000)
+    my_posts.append(post.dict())
     return {"data": post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    print(type(id))
+    if not post:
+        return {"error": "Post not found"}
+    return {"post_details": post}
