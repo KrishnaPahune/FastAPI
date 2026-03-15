6bb08e8d8cbd6505defce139a040a2bdd56886b2
Krishna Pahune <krishnapahune4@gmail.com>
2026-03-15 12:25:09 +0530

    CRUD operations for a simple application.

--- app/main.py
+++ app/main.py
@@ -11,8 +11,6 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
-    id: Optional[int] = None
-
 
 my_posts = [
     {"title": "title of post 1", "content": "content of post 1", "id": 1},
@@ -39,11 +37,14 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts",status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post.id = random.randint(0, 100000000)
-    my_posts.append(post.dict())
-    return {"data": post}
+    post_dict = post.model_dump()
+    post_dict["id"] = random.randint(0, 100000000)
+
+    my_posts.append(post_dict)
+
+    return {"data": post_dict}
 
 @app.get("/posts/latest")
 def get_latest_post():
@@ -65,3 +66,13 @@ def delete_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"post": post_dict}
